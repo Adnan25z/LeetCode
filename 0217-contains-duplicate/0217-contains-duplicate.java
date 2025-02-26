ace95a53bad16eab49ffbfa0f59f77cb41883f5b
Adnan <100885766+Adnan25z@users.noreply.github.com>
2025-02-26 11:01:13 +1100

    Time: 10 ms (88.41%), Space: 58.1 MB (47.17%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,17 +1,12 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Set<Integer> uniqueElements = new HashSet<>();
-
-        // Iterate through each element in the array
-        for (int num : nums) {
-            // Attempt to add the element to the HashSet
-            // If add returns false, the element is a duplicate
-            if (!uniqueElements.add(num)) {
-                return true;  // Duplicate found
+        HashSet<Integer> duplicates = new HashSet<>();
+        for(int num : nums){
+            if(duplicates.contains(num)){
+                return true;
             }
+            duplicates.add(num);
         }
-
-        // No duplicates found
         return false;
     }
 }
